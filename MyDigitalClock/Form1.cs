4db7d59383a56230d5de469d12d0ba6c29e16de4
Jasmine Graham <thejazzynator@gmail.com>
2022-09-27 21:54:50 -0400

    Add project files.

---
+++ MyDigitalClock/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyDigitalClock
+{
+    public partial class Form1 : Form
+    {
+        decimal timer = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           label1.Text = DateTime.Now.ToString("hh:mm:ss");
+        }
+    }
+}
